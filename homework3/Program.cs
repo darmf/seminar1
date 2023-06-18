5d44ad4f709d7e5dc5a417f4d1abef2689a1837a
Daria_Fomina <daria.m.fomina@mail.ru>
2023-06-18 14:18:23 +0300

    Добавили решение третьей задачи

---
+++ homework3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдает,  является ли оно четным
+
+Console.Write("введите число");
+int numb = int.Parse(Console.ReadLine()!);
+if(numb%2 == 0)
+{
+    Console.Write(" число четное");
+}
+else
+{
+    Console.Write(" число нечетное");
+}
+
