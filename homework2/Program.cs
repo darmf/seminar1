2da5efa500f6f0ae4b6bf04bbf8f96bfc5eb31b2
Daria_Fomina <daria.m.fomina@mail.ru>
2023-06-18 13:46:09 +0300

    Добавили решение второй задачи

---
+++ homework2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход 3 числа и выдает максимальное из них
+
+Console.Write("введите первое число ");
+int numb1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("введите второе число");
+int numb2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("введите третье число");
+int numb3 = int.Parse(Console.ReadLine()!);
+
+int max = 0;
+
+if(numb1 > max)
+{
+   max = numb1;
+}
+if(numb2 > max)
+{
+    max = numb2;
+}
+if(numb3 > max)
+{
+    max = numb3;
+}
+
+Console.WriteLine($"Самое большое число — {max}");
+
+
+
