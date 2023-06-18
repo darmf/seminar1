ebe0cb38b9f0da20e7dab4c0aa533845def05c3c
Daria_Fomina <daria.m.fomina@mail.ru>
2023-06-18 14:54:59 +0300

    Добавили ршение четвертой задачи

---
+++ homework4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу,которая на вход принимает чсило N, а на выходе показывает все четные числа от 1 до  N
+
+Console.Write("введите число N" );
+ int number1 = int.Parse( Console.ReadLine()!);
+
+int number2 = 2;
+
+if(number1 > 1)
+{
+    while(number2 <= number1)
+    {
+        Console.Write(number2 + " ");
+        number2 = number2 + 2;
+    }
+}
